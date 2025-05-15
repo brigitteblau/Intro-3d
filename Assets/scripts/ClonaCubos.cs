cf9d039fc6f1cc5467e5c22a444015165a8268da
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-05-15 12:12:41 -0300

    q

---
+++ Assets/scripts/ClonaCubos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonaCubos : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject cuboPrefab;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Clonar()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
